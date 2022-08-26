09c5d45395d4267514c1a6726cceffeff0ead8c3
Sistema Financeiro - Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-08-26 13:10:31 -0300

    KESSEF26082022105139FEATURE

---
+++ kessef/modelagem_dados/modelagem_funcionalidade_gerenciador_pessoa_conta_bancaria_cartao_bancario.sql
@@ -0,0 +1,77 @@
+/*
+    Gerenciador de Pessoas
+*/
+
+drop table if exists tb_tipo_pessoa cascade;
+create table tb_tipo_pessoa(
+    codigo serial primary key,
+    descricao varchar(100) unique not null
+);
+
+drop table if exists tb_pessoa cascade;
+create table tb_pessoa(
+    codigo serial primary key,
+    id_tipo_pessoa integer not null,
+    nome varchar(100) unique not null,
+    foreign key (id_tipo_pessoa) references tb_tipo_pessoa (codigo)
+);
+
+/*
+    Gerenciador de Conta Bancária
+*/
+
+drop table if exists tb_instituicao_bancaria cascade;
+create table tb_instituicao_bancaria(
+    codigo serial primary key,
+    id_pessoa integer not null,
+    numero_banco varchar(3) unique not null,
+    foreign key (id_pessoa) references tb_pessoa (codigo)
+);
+
+drop table if exists tb_tipo_conta_bancaria cascade;
+create table tb_tipo_conta_bancaria(
+    codigo serial primary key,
+    descricao varchar(100) unique not null
+);
+
+drop table if exists tb_conta_bancaria cascade;
+create table tb_conta_bancaria(
+    codigo serial primary key,
+    id_tipo_conta_bancaria integer not null,
+    id_instituicao_bancaria integer not null,
+    id_pessoa_titular_conta integer not null,
+    numero_agencia varchar(4) unique not null,
+    numero_conta varchar(100) unique not null,
+    data_abertura date not null,
+    data_encerramento date not null,
+    saldo double precision not null,
+    data_atualizacao date not null,
+    usuario_atualizacao varchar(100) not null,
+    foreign key (id_tipo_conta_bancaria) references tb_tipo_conta_bancaria (codigo),
+    foreign key (id_instituicao_bancaria) references tb_instituicao_bancaria (codigo),
+    foreign key (id_pessoa_titular_conta) references tb_pessoa (codigo)
+);
+
+/*
+    Gerenciador de Cartão Bancário
+*/
+
+drop table if exists tb_funcao_cartao_bancario cascade;
+create table tb_funcao_cartao_bancario(
+    codigo serial primary key,
+    descricao varchar(100) unique not null
+);
+
+drop table if exists tb_cartao_bancario cascade;
+create table tb_cartao_bancario(
+    codigo serial primary key,
+    id_conta_bancaria integer not null,
+    id_funcao_cartao_bancario integer not null,
+    data_recebimento date not null,
+    data_liberacao date not null,
+    data_expiracao date not null,
+    limite double precision not null,
+    dia_vencimento date not null,
+    foreign key (id_conta_bancaria) references tb_conta_bancaria (codigo),
+    foreign key (id_funcao_cartao_bancario) references tb_funcao_cartao_bancario (codigo)
+);
